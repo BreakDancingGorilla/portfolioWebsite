2c3d2a4b2d7f39a9b781a81a185794722d5e337e
BreakDancingGorilla <gabrielosborn1029@gmail.com>
2026-05-03 17:44:53 +0000

    update

--- code.js
+++ code.js
@@ -6,6 +6,13 @@ window.onfocus = function () {
   document.title = "My Normal Website";
 }
     
+let msg = "  Wait, did you really just leave me for another tab?  ";
+function scrollTitle() {
+  document.title = msg;
+  msg = msg.substring(1) + msg.substring(0, 1);
+  setTimeout(scrollTitle, 150); // Lower is faster
+}
+scrollTitle();
 
 
 //For on load.
