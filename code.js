be7929447a527b3774e7103fca08ad0e1d09ea38
BreakDancingGorilla <gabrielosborn1029@gmail.com>
2026-05-03 17:43:57 +0000

    update

--- code.js
+++ code.js
@@ -1,5 +1,10 @@
  addEventListener("load", (event) => {
-
+window.onblur = function () {
+  document.title = "I miss you... come back! 🥺";
+}
+window.onfocus = function () {
+  document.title = "My Normal Website";
+}
     
 
 
